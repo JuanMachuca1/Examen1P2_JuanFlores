f59cc025a1beaf3b105834dd6fa15baeca4be248
JuanMachuca1 <juandavidflores2005@gmail.com>
2023-08-18 14:00:49 -0600

    clase equipo y estadio

--- src/examen1p2_juanflores/Estadio.java
+++ src/examen1p2_juanflores/Estadio.java
@@ -4,10 +4,59 @@
  */
 package examen1p2_juanflores;
 
-/**
- *
- * @author JD
- */
+
 public class Estadio {
+    private String nombreEs;
+    private String ciudad;
+    private int capacidad;
+    private Equipo equipo = new Equipo();
+
+    public Estadio() {
+    }
+
+    public Estadio(String nombreEs, String ciudad, int capacidad) {
+        this.nombreEs = nombreEs;
+        this.ciudad = ciudad;
+        this.capacidad = capacidad;
+    }
+
+    
+    public String getNombreEs() {
+        return nombreEs;
+    }
+
+    public void setNombreEs(String nombreEs) {
+        this.nombreEs = nombreEs;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Estadio{" + "nombreEs=" + nombreEs + ", ciudad=" + ciudad + ", capacidad=" + capacidad + ", equipo=" + equipo + '}';
+    }
+    
     
 }
